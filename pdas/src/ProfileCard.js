fefdc3a751f6f576024a6103ec9110b0d58e5877
RaudoMoquete <sanchezz09@hotmail.com>
2024-11-23 13:02:18 -0500

    appliying js destructuring feature on ProfileCard

--- pdas/src/ProfileCard.js
+++ pdas/src/ProfileCard.js
@@ -1,5 +1,21 @@
-function ProfileCard() {
-    return <div>ProfileCard</div>
+function ProfileCard({ title, handle, image }) {
+    //function ProfileCard(props) {
+    // before
+    /* const title = props.title;
+    const handle = props.handle */
+
+    //javaScript destructuring feature, tambien se puede implementar arriba justo como está ahora
+    //const { title, handle } = props;
+    
+    return (
+        <div>
+            <img src={image} alt="pds logo" />
+            <div>Title is {title}</div>
+            <div>Handle is {handle}</div>
+      {/*       <div>Title is props.title</div>
+            <div>Handle is props.handle</div> */}
+        </div>
+    );
 }
 
 export default ProfileCard;
