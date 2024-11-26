d1367084f9d55a94ee0a9ce4b8a882fcf62a2648
RaudoMoquete <sanchezz09@hotmail.com>
2024-11-26 18:29:27 -0500

    feat: adding random animals, some custom css, and aplying array destructuring

--- animals/src/App.js
+++ animals/src/App.js
@@ -1,7 +1,39 @@
+import './App.css';
+import { useState } from "react";
+import AnimalShow from "./AnimalShow";
+
+function getRandomAnimal() {
+    const animals = ['bird', 'cat', 'cow', 'dog', 'gator', 'horse'];
+
+    return animals[Math.floor(Math.random() * animals.length)]
+}
+
+console.log(getRandomAnimal());
+
 function App() {
-    return <div>
-        Show animal list here
-    </div>
+    const [animals, setAnimals] = useState([]);
+
+    const handleClick = () => {
+        setAnimals([...animals, getRandomAnimal()])
+        //eso se pudo hacer de esta manera:
+        //animals.push(getRandomAnimal())
+        //PERO ESO MODIFICA UNA PIEZA DE ESTADO DIRECTAMENTE, Y ESO NO ES DEBIDO
+    };
+
+    const renderAnimals = animals.map((animal, index) => {
+        return <AnimalShow type={animal} key={index} />
+
+        //en este casi map fuera algo como esto:
+   /*      <AnimalShow type={animals[0]} />
+        <AnimalShow type={animals[1]} /> */
+    });
+
+    return (
+        <div className='app'>
+            <button onClick={handleClick}>Add Animal</button>
+            <div>{renderAnimals}</div>
+        </div>
+    );
 }
 
 export default App;
